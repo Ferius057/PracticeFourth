0444f8039bb1c9eba03d58a30440caa968c33f54
Ferius057 <ferius057@gmail.com>
2023-10-04 22:51:23 +0600

    add task2

---
+++ Task2/src/main/java/kz/ferius_057/task2/Main.java
@@ -0,0 +1,41 @@
+package kz.ferius_057.task2;
+
+import com.google.common.io.Files;
+import lombok.val;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
+
+/**
+ * @author Ferius_057 (Charles_Grozny)
+ * @date ⭐ 04.10.2023 | 22:29 ⭐
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String fileName = "article.txt";
+        int linesToRead = 234;
+
+        readLast(linesToRead, Paths.get(fileName).toFile());
+    }
+
+    public static void readLast(int lines, File file) throws IOException {
+        if (lines <= 0) {
+            System.out.println("Кол-во строк должно быть положительным цельным числом");
+            return;
+        }
+
+        val listLines = Files.readLines(file, Charset.defaultCharset());
+        val totalLines = listLines.size();
+
+        if (totalLines <= lines) {
+            System.out.println("В файле меньше строк чем нужно прочесть");
+        } else {
+            System.out.printf("Последние строки из файла в кол-ве %s строк: %n", lines);
+            for (int i = totalLines - lines; i < totalLines; i++) {
+                System.out.println(listLines.get(i));
+            }
+        }
+    }
+}
